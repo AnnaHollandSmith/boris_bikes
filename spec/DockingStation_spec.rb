1171b5e9cd033fd4f5f361cae8f3dacb2f7c4afd
riyapabari <riya.pabari@gmail.com>
2016-05-27 11:31:04 +0100

    Tidied up DockingStation_sec file

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -9,26 +9,65 @@ describe DockingStation do
   it {is_expected.to respond_to (:bikes)}
 
   describe '#release_bike' do
-    it 'releases a bike if bikes are available' do
-      #bike = Bike.new
-      bike = double(:bike)
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
-    end
-    it 'raises an error if no bikes available' do
-      expect{subject.release_bike}.to raise_error("No bikes available!")
+    describe 'releasing when there are no broken bikes' do
+      it 'releases a bike if bikes are available' do
+        #bike = Bike.new
+        bike = double(:bike)
+        subject.dock(bike)
+        expect(subject.release_bike).to eq bike
+      end
+      it 'raises an error if no bikes available' do
+        expect{subject.release_bike}.to raise_error("No bikes available!")
+      end
+     end 
+    describe 'releasing when there are some broken bikes' do
+      it 'doesn\'t release latest bike when latest bike is broken' do
+        station = DockingStation.new(1)
+        #bike = Bike.new
+        bike = double(:bike)
+        bike.report_broken
+        station.dock(bike)
+        expect{station.release_bike}.to raise_error("Sorry, there are no working bikes available!")
+      end
+      it 'releases a bike when there are working bikes and the last one is broken' do
+        station = DockingStation.new
+        bike1 = double(:bike1)
+        bike2 = double(:bike2)
+        bike1.report_broken
+        station.dock(bike2)
+        station.dock(bike1)
+        expect{station.release_bike}.not_to raise_error
+      end
+      it 'raises error already removed all working bikes' do
+        station = DockingStation.new
+        bike1 = double(:bike1)
+        bike2 = double(:bike2)
+        bike1.report_broken
+        station.dock(bike2)
+        station.dock(bike1)
+        station.release_bike
+        expect{station.release_bike}.to raise_error("Sorry, there are no working bikes available!")
+      end
     end
   end
 
-  it 'releases working bikes' do
-    #bike = Bike.new
-    bike = double(:bike)
-    subject.dock(bike)
-    subject.release_bike
-	  expect(bike).to be_working
-	end
+  describe 'working?' do
+    it 'releases working bikes' do
+      #bike = Bike.new
+      bike = double(:bike)
+      subject.dock(bike)
+      subject.release_bike
+  	  expect(bike).to be_working
+  	end
+  end
     
   describe '#dock(bike)' do
+    it 'returns docked bike' do 
+      #bike = Bike.new
+      bike = double(:bike)
+      subject.dock(bike)
+      expect(subject.bikes.last).to eq bike
+    end
     it 'docks the bike when there is space available' do 
       #bike = Bike.new
       bike = double(:bike)
@@ -52,13 +91,6 @@ describe DockingStation do
     end
   end
 
-  it 'returns docked bike' do 
-  	#bike = Bike.new
-    bike = double(:bike)
-  	subject.dock(bike)
-  	expect(subject.bikes.last).to eq bike
-  end
-
   describe '#initialize' do
     it 'initialises with one argument' do
       expect{DockingStation.new(30)}.not_to raise_error
@@ -66,39 +98,6 @@ describe DockingStation do
     it 'initialises with default capacity' do
       expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
     end
-  end
-
-  describe 'releasing when there are some broken bikes' do
-    it 'doesn\'t release latest bike when latest bike is broken' do
-      station = DockingStation.new(1)
-      #bike = Bike.new
-      bike = double(:bike)
-      bike.report_broken
-      station.dock(bike)
-      expect{station.release_bike}.to raise_error("Sorry, there are no working bikes available!")
-    end
-
-    it 'releases a bike when there are working bikes and the last one is broken' do
-      station = DockingStation.new
-      bike1 = double(:bike1)
-      bike2 = double(:bike2)
-      bike1.report_broken
-      station.dock(bike2)
-      station.dock(bike1)
-      expect{station.release_bike}.not_to raise_error
-    end
-
-    it 'raises error already removed all working bikes' do
-      station = DockingStation.new
-      bike1 = double(:bike1)
-      bike2 = double(:bike2)
-      bike1.report_broken
-      station.dock(bike2)
-      station.dock(bike1)
-      station.release_bike
-      expect{station.release_bike}.to raise_error("Sorry, there are no working bikes available!")
-    end
-
   end
 
 end

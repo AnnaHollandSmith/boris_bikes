97649512650eb67b07acbe8098cb525cba784aed
riyapabari <riya.pabari@gmail.com>
2016-05-27 09:52:29 +0100

    Completed challenge 19: added doubles in rspec

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -10,7 +10,8 @@ describe DockingStation do
 
   describe '#release_bike' do
     it 'releases a bike if bikes are available' do
-      bike = Bike.new
+      #bike = Bike.new
+      bike = double(:bike)
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
@@ -20,7 +21,8 @@ describe DockingStation do
   end
 
   it 'releases working bikes' do
-    bike = Bike.new
+    #bike = Bike.new
+    bike = double(:bike)
     subject.dock(bike)
     subject.release_bike
 	  expect(bike).to be_working
@@ -28,29 +30,31 @@ describe DockingStation do
     
   describe '#dock(bike)' do
     it 'docks the bike when there is space available' do 
-      bike = Bike.new
+      #bike = Bike.new
+      bike = double(:bike)
       expect(subject.dock(bike).last).to eq bike
     end
     it 'docks 20 bikes when capacity is set to default' do
-      expect{DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new)}}.not_to raise_error
+      expect{DockingStation::DEFAULT_CAPACITY.times {subject.dock(double(:bike))}}.not_to raise_error
     end
     it 'raises an error when there is no space available when capacity is set to default' do
-      DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new)}
-      expect{subject.dock(Bike.new)}.to raise_error("No space available!")
+      DockingStation::DEFAULT_CAPACITY.times {subject.dock(double(:bike))}
+      expect{subject.dock(double(:bike))}.to raise_error("No space available!")
     end
     it 'docks 50 bikes when capacity is set to 50' do
       station = DockingStation.new(50)
-      expect{50.times {station.dock(Bike.new)}}.not_to raise_error
+      expect{50.times {station.dock(double(:bike))}}.not_to raise_error
     end
     it 'raises an error at 51 bikes when capacity is set to 50' do
       station = DockingStation.new(50)
-      50.times {station.dock(Bike.new)}
-      expect{station.dock(Bike.new)}.to raise_error("No space available!")
+      50.times {station.dock(double(:bike))}
+      expect{station.dock(double(:bike))}.to raise_error("No space available!")
     end
   end
 
   it 'returns docked bike' do 
-  	bike = Bike.new
+  	#bike = Bike.new
+    bike = double(:bike)
   	subject.dock(bike)
   	expect(subject.bikes.last).to eq bike
   end
@@ -67,7 +71,8 @@ describe DockingStation do
   describe 'releasing when there are some broken bikes' do
     it 'doesn\'t release latest bike when latest bike is broken' do
       station = DockingStation.new(1)
-      bike = Bike.new
+      #bike = Bike.new
+      bike = double(:bike)
       bike.report_broken
       station.dock(bike)
       expect{station.release_bike}.to raise_error("Sorry, there are no working bikes available!")
@@ -75,8 +80,8 @@ describe DockingStation do
 
     it 'releases a bike when there are working bikes and the last one is broken' do
       station = DockingStation.new
-      bike1 = Bike.new
-      bike2 = Bike.new
+      bike1 = double(:bike1)
+      bike2 = double(:bike2)
       bike1.report_broken
       station.dock(bike2)
       station.dock(bike1)
@@ -85,8 +90,8 @@ describe DockingStation do
 
     it 'raises error already removed all working bikes' do
       station = DockingStation.new
-      bike1 = Bike.new
-      bike2 = Bike.new
+      bike1 = double(:bike1)
+      bike2 = double(:bike2)
       bike1.report_broken
       station.dock(bike2)
       station.dock(bike1)
